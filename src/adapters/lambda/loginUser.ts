8a36a15adafbc67ff0b94e5bd185034d3a744cd4
margaretjgu <margaret.j.gu@gmail.com>
2024-01-29 23:04:40 -0500

    add(fix deployment, check for types)

--- src/adapters/lambda/loginUser.ts
+++ src/adapters/lambda/loginUser.ts
@@ -1,13 +1,22 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
-import { loginUser } from '../../application/use_cases/loginUser';
+import { loginUser } from 'src/application/use_cases/loginUser';
 
 export const handler: APIGatewayProxyHandler = async (event) => {
-  const { email, password } = JSON.parse(event.body);
-
   try {
-    const token = await loginUser(email, password);
-    return { statusCode: 200, body: JSON.stringify({ token }) };
+    if (typeof event.body === 'string') {
+      const { email, password } = JSON.parse(event.body);
+
+      if (!email || !password) {
+        return { statusCode: 400, body: JSON.stringify({ error: 'Email and password are required' }) };
+      }
+
+      const token = await loginUser(email, password);
+      return { statusCode: 200, body: JSON.stringify({ token }) };
+    } else {
+      return { statusCode: 400, body: JSON.stringify({ error: 'Bad request' }) };
+    }
   } catch (error) {
+    console.error(error);
     return { statusCode: 401, body: JSON.stringify({ error: 'Login failed' }) };
   }
 };
--- src/adapters/lambda/registerUser.ts
+++ src/adapters/lambda/registerUser.ts
@@ -1,19 +1,26 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
-import { registerUser } from 'src/application/use_cases/registerUser';
+import { registerUser } from '../../application/use_cases/registerUser';
 import { v4 as uuidv4 } from 'uuid';
 
 export const handler: APIGatewayProxyHandler = async (event) => {
+  if (typeof event.body !== 'string') {
+    return { statusCode: 400, body: JSON.stringify({ error: 'Request body must be a valid JSON string' }) };
+  }
+
   const { username, name, email, password } = JSON.parse(event.body);
-  // Input validation logic (pseudo code)
+
   if (!username || !email || !password || !name) {
     return { statusCode: 400, body: JSON.stringify({ error: 'Missing required fields' }) };
   }
 
   try {
-    const userId = uuidv4(); 
-    const token = await registerUser(username, name, email, password, userId); 
-    return { statusCode: 200, body: JSON.stringify({ username, token }) };
+    const userId = uuidv4();
+    const token = await registerUser(username, name, email, password, userId);
+    return { statusCode: 200, body: JSON.stringify({ token, userId }) };
   } catch (error) {
-    return { statusCode: 500, body: JSON.stringify({ error: error.message }) };
+    if (error instanceof Error) {
+      return { statusCode: 500, body: JSON.stringify({ error: error.message }) };
+    }
+    return { statusCode: 500, body: JSON.stringify({ error: 'An unknown error occurred' }) };
   }
 };
